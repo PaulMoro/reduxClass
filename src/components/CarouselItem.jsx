72d29768c22d70c9738e7c5bfcb43b77e6badb29
Paul Montoya <paulmontoya15@gmail.com>
2021-02-24 20:20:39 -0500

    Class 16 finished

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -7,11 +7,12 @@ import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
 const CarouselItem = (props) => {
-  const { id, cover, title, year, contentRating, duration } = props
+  const { id, cover, title, year, contentRating, duration, isList } = props
   const handleSetFavorite = () => {
     props.setFavorite ({
         id, cover, title, year, contentRating, duration
     })
+
   }
   const handleDeliteFavorite = (itemId) => {
     props.deliteFavorite(itemId)
@@ -22,18 +23,23 @@ const CarouselItem = (props) => {
       <div className="carousel-item__details">
         <div>
           <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
-          <img 
-            className="carousel-item__details--img"
-            src={plusIcon}
-            alt="Plus Icon"
-            onClick={handleSetFavorite}
-          />
-          <img 
-            className="carousel-item__details--img"
-            src={playIcon}
-            alt="Play Icon"
-            onClick= {() => handleDeliteFavorite(id)}
-            />
+{/* Esta es la validacion que tenemos para que el boton de agregar a favoritos 
+se desactive cuando esté ya en favoritos y aparesca la de eliminar, que en este
+caso seria otro playIcon */}
+          { isList ?
+            <img 
+              className="carousel-item__details--img"
+              src={playIcon}
+              alt="Play Icon"
+              onClick= {() => handleDeliteFavorite(id)}
+              /> :
+              <img 
+                className="carousel-item__details--img"
+                src={plusIcon}
+                alt="Plus Icon"
+                onClick={handleSetFavorite}
+              /> 
+            }
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">
