a8087c0a3d55b530350eb46f20ddd6b5f35de3d4
Paul Montoya <paulmontoya15@gmail.com>
2021-02-24 20:43:11 -0500

    Class 17 finished

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { loginRequest } from '../actions'
 import '../assets/styles/components/Login.scss'
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png'
 
-const Login = () => {
+const Login = (props) => {
 	const [form, setValues] = useState ({
 		email:'',
 	});
@@ -18,6 +20,8 @@ const Login = () => {
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
+		props.loginRequest(form);
+		props.history.push('/')
 		console.log(form);
 	};
 
@@ -68,5 +72,9 @@ const Login = () => {
 	)
 };
 
+const mapDispachToProps = {
+	loginRequest,
+};
 
-export default Login;
+
+export default connect(null, mapDispachToProps)(Login);
